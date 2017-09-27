82ea41e5619df4373e20d95a5d3872ecca09fd2a
Kristy Peterson <kpeterson.kap@gmail.com>
2017-09-27 22:54:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,9 @@ GROUP BY users.name ORDER BY total_pledges, users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) FROM projects
+INNER JOIN pledges ON pledges.user_id = users.id
+WHERE projects.category = music;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
