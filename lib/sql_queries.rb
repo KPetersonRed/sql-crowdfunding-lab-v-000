6400c8369703346ca2ed46d30bfd067949249730
Kristy Peterson <kpeterson.kap@gmail.com>
2017-09-27 22:58:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, SUM(pledges.amount) FROM projects
-INNER JOIN pledges ON pledges.user_id = users.id
+INNER JOIN pledges ON pledges.project_id = projects.id
 WHERE projects.category = 'music';"
 end
 
